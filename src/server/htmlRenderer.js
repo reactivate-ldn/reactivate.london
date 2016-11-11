8d54cf101c4d6310636642ff868163d69887318a
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2016-11-11 11:13:18 +0000

    Update HTML markup

--- src/server/htmlRenderer.js
+++ src/server/htmlRenderer.js
@@ -19,16 +19,22 @@ const renderHtml = (getBundle, appendHead) => {
     <html lang="en">
       <head>
         <title>Reactivate London</title>
+
         <meta charset="UTF-8">
-        <meta name="description" content="A brand new meetup for all things React, JavaScript and Functional (Reactive) Programming!">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+        <meta name="author" content="Reactivate London">
+        <meta name="description" content="A brand new meetup for all things React, JavaScript and Functional (Reactive) Programming!">
+        <meta name="keywords" content="reactivate,london,meetup,react,javascript,frp">
+        <meta name="application-name" content="Reactivate London">
 
         ${appendHead}
         <style type="text/css">${css}</style>
       </head>
       <body>
         <div id="root">${html}</div>
-        <script type="text/javascript" src="/static/bundle.js"></script>
+        <script type="text/javascript" async src="/static/bundle.js"></script>
       </body>
     </html>
   `
