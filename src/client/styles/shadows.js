1fd98862fa86a3b43b333520a7d552df74cbdc48
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-02-05 00:53:43 +0000

    Add toString to shadow colors

--- src/client/styles/shadows.js
+++ src/client/styles/shadows.js
@@ -5,6 +5,6 @@ const baseShallowShadow = `0 0 ${rem(24)} ${black.opacity(.22)}`
 const baseDiffuseShadow = `0 ${rem(24)} ${rem(24)} ${black.opacity(.3)}`
 
 export const diffuseShadow = `${baseShallowShadow}, ${baseDiffuseShadow}`;
-export const shallowShadow = `0 ${rem(2)} ${rem(4)} ${black.opacity(.4)}`;
-export const insetShadow = `inset 0 ${rem(17)} ${rem(35)} ${black.opacity(.23)}`;
+export const shallowShadow = `0 ${rem(2)} ${rem(4)} ${black.opacity(.4).toString()}`;
+export const insetShadow = `inset 0 ${rem(17)} ${rem(35)} ${black.opacity(.23).toString()}`;
 
