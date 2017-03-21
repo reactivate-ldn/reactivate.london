5238683462009cf147d7eb3aed12c51dc6deda2e
Phil Plueckthun <phil@plckthn.me>
2017-03-21 23:39:20 +0000

    Add missing files and update page

--- pages/_document.js
+++ pages/_document.js
@@ -72,12 +72,12 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/static/favicon.png" />
 
           <title>Reactivate London</title>
 
-          <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
 
           <meta name="author" content="Reactivate London" />
