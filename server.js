d48a2b1e3ec349b7c8fec83a7b0cfc7b0587780a
Phil Plueckthun <phil@plckthn.me>
2017-03-22 00:15:04 +0000

    Add preact and ssr cache

---
+++ server.js
@@ -0,0 +1,71 @@
+const dev = process.env.NODE_ENV !== 'production'
+const moduleAlias = require('module-alias')
+
+if (!dev) {
+  moduleAlias.addAlias('react', 'preact-compat')
+  moduleAlias.addAlias('react-dom', 'preact-compat')
+}
+
+const { parse } = require('url')
+const express = require('express')
+const LRUCache = require('lru-cache')
+const next = require('next')
+
+const app = next({ dir: '.', dev })
+const handle = app.getRequestHandler()
+
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 * 24 // 24h
+})
+
+const cachedRender = (req, res, pagePath, queryParams) => {
+  const key = `${req.url}`
+
+  if (ssrCache.has(key)) {
+    res.append('X-Cache', 'HIT')
+    res.send(ssrCache.get(key))
+    return
+  }
+
+  app.renderToHTML(req, res, pagePath, queryParams)
+    .then(html => {
+      ssrCache.set(key, html)
+
+      res.append('X-Cache', 'MISS')
+      res.send(html)
+    })
+    .catch((err) => {
+      app.renderError(err, req, res, pagePath, queryParams)
+    })
+}
+
+const PORT = process.env.PORT || 3000
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.disable('x-powered-by')
+
+    server.get('/', (req, res) => {
+      cachedRender(req, res, '/')
+    })
+
+    server.get('/past-speakers', (req, res) => {
+      cachedRender(req, res, '/past-speakers')
+    })
+
+    server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true)
+      handle(req, res, parsedUrl)
+    })
+
+    server.listen(PORT, err => {
+      if (err) {
+        throw err
+      }
+
+      console.log(`> Ready on http://localhost:${PORT}`)
+    })
+  })
